7d2f9b198015b85e8f759a3b42d05179d477d3c5
divya <dk291996@gmail.com>
2020-10-27 01:49:55 +0530

    changes

--- user/help_function.py
+++ user/help_function.py
@@ -1,6 +1,7 @@
 import json
 
 def getrequest(request):
+    print(request)
     if request.content_type == "application/json":
         data = json.loads(request.body)
     else:
--- user/urls.py
+++ user/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import *
+from .views import LoginApi
 
 urlpatterns = [
     path('login/', LoginApi.as_view()),
